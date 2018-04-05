fe641b15d430a184b0c3c03dcca93bddf09b8bf7
ellieallien <35920142+ellieallien@users.noreply.github.com>
2018-04-05 18:13:11 +0200

    arghhhh (confint 2/2)

--- constructionsite/case_function_test.R
+++ constructionsite/case_function_test.R
@@ -154,14 +154,22 @@ hypothesis_test_chisquare <- function(dependentvar, independent, design, data = 
   return(b)
   }
   
+
+independent.var <- "hh_profile/hhh_sex"
+
+# throws error "all variables must be in design = argument
 hypothesis_test_one_sample_z_num <- function(data.dependent.var, crit, design, data = data) {
-  svyttest(data.dependent.var~1, null.value = crit, design = design)
+  svyttest(data[[dependentvar]]~data[[independent.var]], design = design, family = quasibinomial())
 }
 
 
+svyttest(data$`hh_profile/hhh_age` ~ data$`hh_profile/hhh_sex`, design = design)
+
+#very weird that the confint function spits out 2.5% and 97.5%
+#trying to fix it to no avail
 confidence_intervals_num <- function(dependentvar, design, data = data){
   summary <- svymean(data[[dependentvar]], design, na.rm = T)
-  confint(svymean(data[[dependentvar]] + data[[dependentvar]], design, na.rm = T))
+  confint(svymean(data[[dependentvar]] + data[[dependentvar]], design, na.rm = T), level = 0.95)
   }
 
 
