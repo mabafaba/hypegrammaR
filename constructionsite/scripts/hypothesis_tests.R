bb15c652d8babde3a3b76172fa61de5ed9bf8674
Martin B <barner.mar@gmail.com>
2018-05-15 12:43:34 +0200

    empty tests

--- constructionsite/scripts/hypothesis_tests.R
+++ constructionsite/scripts/hypothesis_tests.R
@@ -21,3 +21,51 @@ hypothesis_test_chisquared <- function(independent.var = independent.var,
 
 
 
+hypothesis_test_t <- function(independent.var = independent.var,
+                                       dependent.var = data.dependent.var,
+                                       design){
+  formula_string<-paste0("~",independent.var, "+", dependent.var)
+  chisq <- svychisq (formula(formula_string), design, na.rm = TRUE)
+  results<-list()
+  results$test.results <- c(chisq$statistic, chisq$p.value)
+  results$test.parameters <- c(chisq$parameter, chisq$method)
+  results$hypothesis_test<-"chi-squared test of independence"
+  return(results)
+}
+
+
+
+
+
+hypothesis_test_z <- function(independent.var = independent.var,
+                               dependent.var = data.dependent.var,
+                               design){
+  # .....
+  
+  
+  results<-list()
+  results$test.results <- c()
+  results$test.parameters <- c()
+  results$hypothesis_test<-""
+  return(results)
+}
+
+
+hypothesis_test_linear_regression <- function(independent.var = independent.var,
+                              dependent.var = data.dependent.var,
+                              design){
+
+  
+  # .....
+  
+  
+  results<-list()
+  results$test.results <- c()
+  results$test.parameters <- c()
+  results$hypothesis_test<-""
+  return(results)
+}
+
+
+
+
