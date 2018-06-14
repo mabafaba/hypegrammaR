ea3a9d24abc3351b087876671e738e93de05bae4
Martin B <barner.mar@gmail.com>
2018-06-14 15:18:15 +0200

    self containted files

--- test_data/bgd/R_example_composite_indicator_functions.R
+++ test_data/bgd/R_example_composite_indicator_functions.R
@@ -1,15 +1,20 @@
 # Internal function that recodes each element (variable) of the composite indicator into a vector with value = weight if the critical value is met and 0 otherwise
 recode.elements <- function(vector, critical.value, weight){
-  recoded.v <- vector %in% critical.value ## recodes the vector into T/F depending on whether the value matches the critical value. 
+  recoded.v <- vector %in% critical.value ## recodes the vector into T/F depending on whether the value matches the critical value.
   recoded.v[recoded.v == TRUE] <- weight
   recoded.v[recoded.v == FALSE] <- 0
   return(recoded.v)
 }
 
-# Builds the composite indicator by summing the recoded individual elements. 
-compose_indicator <- function(data, parameter, critical.value, weight){
-  data_ci <- data[match(parameter, names(data))] #take a subset of the data with only the columns needed for the composite indicator
-  value_per_indicator <- mapply(recode.elements, data_ci, critical.value, weight) %>% as.data.frame
-  indicator <- rowSums(value_per_indicator) / max(rowSums(value_per_indicator)) #Divides by the maximum to get a composite indicator >1
-  return(indicator)}
+# Builds the composite indicator by summing the recoded individual elements.
+compose_indicator_binarised_weighted <- function(data, variables, critical.values, weights){
+
+  data_ci <- data[match(variables, names(data))] #take a subset of the data with only the columns needed for the composite indicator
+  value_per_indicator <- mapply(recode.elements, data_ci, critical.values, weights) %>% as.data.frame
+  indicator <- rowSums(value_per_indicator) / max(rowSums(value_per_indicator)) #Divides by the maximum to get a composite indicator <1
+  return(indicator)
+
+  }
+
+
 
--- test_data/bgd/R_example_composite_indicators_execute.R
+++ test_data/bgd/R_example_composite_indicators_execute.R
@@ -1,8 +1,8 @@
-################## 
-### This script executes the composite indicator calculation, 
+##################
+### This script executes the composite indicator calculation,
 ### using the functions defined in "R_example_composite_indicator_functions.R"
 
-################# 
+#################
 ###Load and prepare the data
 rm(list=ls())
 this_script_path<-(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -12,31 +12,22 @@ getwd()
 source("./test_data/bgd/bgd_analysis_data_load_and_prepare.R")
 source("./test_data/bgd/R_example_composite_indicator_functions.R")
 
-data<-load_assessment_bgd()
-################# 
+#################
+data<-read.csv("gws_du_exercise_example_data.csv")
+indicator.definition<-read.csv("./indicator_definition.csv")
+print(indicator.definition)
 
-################# 
-###Make a list of variables entering the composite indicator with their relative weights
-###This can also be replaced by loading a csv file with the parameters, values and weights
-
-parameters <- c("VAR.58...what.is.are.the.problem.s..related.to.the.latrine.if.any..it.is.not.safe..no.door..no.lock..etc..",
-"VAR.99...does.the.shelter.door.have.a.lock.for.the.shelter.to.be.locked.from.the.inside.",
-"VAR.100...is.your.family.sharing.the.shelter.with.another.family.",
-"VAR.101...does.your.family.have.documentation.that.shows.your.address.in.myanmar.",
-"VAR.103...what.are.the.top.three.main.safety.and.security.concerns.for.your.family.",
-"VAR.251...if.you.wanted.to.complain.or.raise.a.problem.related.to.assistance.in.the.camps..where.would.you.go..i.don.t.know") 
-
-critical.values <- c(1, "No", "Yes", "No", "None/no others", 1)# the value the variable should take to be counted (in the same order as the variables)
-weights <- c(0.5, 0.7, 1, 0.5, 1, -1) #the weights in the final indicator associated with taking on that value
-################# 
-
-################# 
-### Execute the compose_indicator function and bind the resulting vector to the data
-composite.indicator <- compose_indicator(data, parameters, critical.values, weights)
+#################
+### calculate the indicator
+composite.indicator <- compose_indicator_weighted_binary(data,
+                                                            variables = indicator.definition$variables,
+                                                            critical.values = indicator.definition$critical.values,
+                                                            weights = indicator.definition$weights)
+# add to the dataframe
 data <- cbind(data, composite.indicator)
 
 #################
 ### Visualise results
 data$composite.indicator %>% table
 hist(data$composite.indicator, breaks = 12)
-
+mean(data$composite.indicator)
